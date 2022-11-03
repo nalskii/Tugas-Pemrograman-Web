74d905b18203422d027100e7bac5c3e100c0bfcb
nalskii <103160634+nalskii@users.noreply.github.com>
2022-11-03 12:56:20 +0700

    Add files via upload

---
+++ task_3/script.js
@@ -0,0 +1,28 @@
+const para = document.createElement("p");
+const text = document.createTextNode("look at it from afar, take your time.");
+para.appendChild(text);
+const element = document.getElementById("div1");
+element.appendChild(para);
+
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
